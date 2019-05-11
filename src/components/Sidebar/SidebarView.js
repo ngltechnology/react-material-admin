ed49b598c55dfe5213b76ebf1a5f3dc0ffed252f
tomoyuki10 <cruis10.katou@gmail.com>
2019-05-11 20:05:49 +0900

    なんかよくわかんないけど、とりあえずコミット

--- src/components/Sidebar/SidebarView.js
+++ src/components/Sidebar/SidebarView.js
@@ -21,8 +21,7 @@ import SidebarLink from './components/SidebarLink/SidebarLinkContainer';
 import Dot from './components/Dot';
 
 const structure = [
-  { id: 0, label: 'Dashboard', link: '/app/dashboard', icon: <HomeIcon /> },
-  //{ id: 1, label: 'Typography', link: '/app/typography', icon: <TypographyIcon /> },
+  { id: 0, label: 'Dashboard', link: '/app/dashboard', icon: <HomeIcon /> }  //{ id: 1, label: 'Typography', link: '/app/typography', icon: <TypographyIcon /> },
   { id: 1, label: 'Data', link: '/app/typography', icon: <TypographyIcon /> },
   { id: 2, label: 'Tables', link: '/app/tables', icon: <TableIcon /> },
   { id: 3, label: 'Notifications', link: '/app/notifications', icon: <NotificationsIcon />},
