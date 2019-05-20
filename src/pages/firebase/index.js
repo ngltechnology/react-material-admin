cc1bc0ffd71df5d7501e52daeb4162bb715682e5
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2019-05-20 18:38:21 +0900

    update firebase setting

--- src/pages/firebase/index.js
+++ src/pages/firebase/index.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app"
 import "firebase/firestore"
+import "firebase/auth"
 import { config } from "./config"
 
 export const firebaseApp = firebase.initializeApp(config)
